8ed113a4aa229cab1b126f1ee86cc3577c4773b6
DNAScanner <main@dnascanner.de>
2024-11-19 19:15:21 +0100

    Base structure done

---
+++ index.ts
@@ -0,0 +1,1 @@
+type Service = "standard-cleaning" | "decluttering" | "office-cleaning" | "basic-cleaning" | "construction-cleaning" | "glass-cleaning" | "household-dissolution" | "bulk-waste-disposal" | "high-pressure-cleaning";
